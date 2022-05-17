1b3695de486044e58d9f71e4ed0e8b411c1c0304
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-05-17 10:58:13 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,7 +7,7 @@ typedef struct Stats
 }Stats;
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
+bool check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
 
 Stats compute_statistics(const float* numberset, int setlength);
 
